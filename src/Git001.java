4f5425da9d60b52335b11b6a8ed47dc1566b2abf
Hbliii <724637652@qq.com>
2021-12-21 16:01:11 +0800

    王涵冰的修改

--- src/Git001.java
+++ src/Git001.java
@@ -1,7 +1,7 @@
 public class Git001 {
     public static void main(String[] args){
 
-        stuinfo("19406040236","王涵冰");
+        stuinfo("19406040236","wanghanbing");
     }
     public static void stuinfo(String sid,String sname){
         System.out.println("学号："+sid + "姓名：" + sname);
